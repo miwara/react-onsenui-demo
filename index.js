a3fbc1db96fd1f138b31caeb73aacdd4aeef751d
miwara <aruim.jon@gmail.com>
2017-07-29 15:02:31 +0900

    bottomtoolbarのデモ

--- index.js
+++ index.js
@@ -4,19 +4,82 @@ import ReactDom from 'react-dom';
 import onsenui from 'onsenui';
 import {
   Page,
-  Button
+  Navigator,
+  Toolbar,
+  List,
+  ListItem
 } from 'react-onsenui';
 
-class App extends React.Component {
-  handleClick() {
-    onsenui.notification.alert({message: 'Hello world!'});
+import BottomToolbarExample from './examples/BottomToolbar';
+
+class Examples extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {class: 'test'};
+    this.getExamples = this.getExamples.bind(this);
+  }
+
+  getExamples() {
+    return [
+      {
+        title: 'Bottom Toolbar',
+        Component: BottomToolbarExample,
+      }
+    ]
+  }
+
+  goto(example) {
+    this.props.navigator.pushPage({
+      component: example.Component,
+      props: {
+        key: example.title
+      }
+    });
   }
 
   render() {
     return (
-        <Page>
-        <Button onClick={this.handleClick}>Push!</Button>
+        <Page style={{background: 'green'}}
+      renderToolbar={() => <Toolbar><div className='center'> Up Toolbar </div></Toolbar>}
+        >
+        <List modifier='inset'
+      dataSource={this.getExamples()}
+      renderHeader={() =>
+          <ListItem lockOnDrag style={{background: 'green'}} tappable tap-background-color='red'> HEADER </ListItem>
+      }
+      renderRow={(example) =>
+          <ListItem key={example.title} onClick={this.goto.bind(this, example)}>{example.title}</ListItem>
+      }
+        />
         </Page>
+    );
+  }
+}
+
+class App extends React.Component {
+  renderPage(route, navigator) {
+    const props = route.props || {};
+    props.navigator = navigator;
+
+    return React.createElement(route.component, route.props);
+  }
+
+  render() {
+    return (
+        <Navigator
+      renderPage={this.renderPage}
+      onPrePush={(e) => console.log('prepush', e)}
+      onPostPush={(e) => console.log('postpush', e)}
+      onPrePop={(e) => console.log('prepot', e)}
+      onPostPop={(e) => console.log('postpop', e)}
+      initialRoute={{
+        component: Examples,
+        props: {
+          key: 'examples'
+        }
+      }}
+      />
     );
   }
 }
